88abd72f7395810d927cd978ca1b94d2f055a417
Ninh Doan <uitnetwork@gmail.com>
2018-01-11 00:10:05 +0800

    Return error message when exception happens

--- cloud-bot-lamda/src/main/kotlin/com/uitnetwork/bot/handler/CloudBotRequestHandler.kt
+++ cloud-bot-lamda/src/main/kotlin/com/uitnetwork/bot/handler/CloudBotRequestHandler.kt
@@ -4,9 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.uitnetwork.bot.config.CloutBotConfig
 import com.uitnetwork.bot.model.ApiGatewayResponse
 import com.uitnetwork.bot.model.ApiGatewayResponse.Companion.fromFulfillmentResponse
+import com.uitnetwork.bot.model.FulfillmentResponse
 import com.uitnetwork.bot.service.FulfillmentRequestParser
 import com.uitnetwork.bot.service.RequestServiceManager
 import mu.KotlinLogging
+import java.util.*
 
 class CloudBotRequestHandler : AbstractSpringAwareHandler<CloutBotConfig>() {
     companion object {
@@ -14,14 +16,21 @@ class CloudBotRequestHandler : AbstractSpringAwareHandler<CloutBotConfig>() {
     }
 
     override fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
-        logger.debug { "Received: ${input["body"]}" }
+        try {
+            logger.debug { "Received: ${input["body"]}" }
 
-        val fulfillmentRequestService = applicationContext.getBean(FulfillmentRequestParser::class.java)
-        val fulfillmentRequest = fulfillmentRequestService.parse(input["body"] as String)
+            val fulfillmentRequestService = applicationContext.getBean(FulfillmentRequestParser::class.java)
+            val fulfillmentRequest = fulfillmentRequestService.parse(input["body"] as String)
 
-        val requestServiceManager = applicationContext.getBean(RequestServiceManager::class.java)
-        val fulfillmentResponse = requestServiceManager.process(fulfillmentRequest)
+            val requestServiceManager = applicationContext.getBean(RequestServiceManager::class.java)
+            val fulfillmentResponse = requestServiceManager.process(fulfillmentRequest)
 
-        return fromFulfillmentResponse(fulfillmentResponse)
+            return fromFulfillmentResponse(fulfillmentResponse)
+        } catch (exception: Exception) {
+            val randomUuidAsErrorId = UUID.randomUUID().toString()
+
+            logger.error(exception) { "Error id: $randomUuidAsErrorId" }
+            return fromFulfillmentResponse(FulfillmentResponse("There is an error while processing the request. Error id: $randomUuidAsErrorId"))
+        }
     }
 }
